0035ee5a43ef515875405fca69b1b8bed598cdd4
Petr Novotnik <pnovotnik@googlemail.com>
2018-01-09 15:10:23 +0100

    Make OrderEventPlaced and the rest of event processing independent

--- order/src/main/java/io/flowing/retail/order/port/adapter/OrderCompletedAdapter.java
+++ order/src/main/java/io/flowing/retail/order/port/adapter/OrderCompletedAdapter.java
@@ -29,7 +29,7 @@ public class OrderCompletedAdapter implements JavaDelegate {
       throw new IllegalStateException("Unknown order: " + orderId);
     }
     long now = System.currentTimeMillis();
-    System.out.println(String.format("Order processed: received: %,dms / process-kickoff: %,dms / payment: %,dms / goods: %,dms / ship: %,dms / total: %,dms",
+    System.out.println(String.format("Order processed: received: %dms / process-kickoff: %dms / payment: %dms / goods: %dms / ship: %dms / total: %dms",
             order.getOrderPlacedRecievedTs() - order.getCreatedTs(),
             order.getRetrievePaymentAdapterTs() - order.getOrderPlacedRecievedTs(),
             order.getFetchGoodsAdapterTs() - order.getRetrievePaymentAdapterTs(),
